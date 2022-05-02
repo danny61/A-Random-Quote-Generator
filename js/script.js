64a9ac4653fd6b5fcab1b64c794c3963259aff12
Daniel <ahmeddaniel18@gmail.com>
2022-05-02 00:29:05 -0400

    initial commit

--- js/script.js
+++ js/script.js
@@ -4,23 +4,30 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-// Check the "Project Resources" section of the project instructions
-// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-const quoteText = document.getElementById('quote')
-const quoteAuthor = document.getElementById('source')
-const getQuoteBtn = document.getElementById('load-quote')
+/*** 
+ * `quotes` array 
+***/
 
-function randomQuote () {
-  fetch ('https://api.quotable.io/random') 
-  .then(response => response.json())
-  .then (data => {
-    quoteText.textContent = data.content;
-    quoteAuthor.textContent = data.author
-  })
-}
 
-randomQuote();
-getQuoteBtn.addEventListener('click', ()=>{
-  randomQuote();
-})
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
