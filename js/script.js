a45ae49c72e9aa033e18cd547d250b473c39c54d
Daniel <ahmeddaniel18@gmail.com>
2022-05-02 01:38:45 -0400

    finished code

--- js/script.js
+++ js/script.js
@@ -10,19 +10,68 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+let quotes = [
+  {
+    quote: "\"Spread love everywhere you go. Let no one ever come to you without leaving happier.\"",
+    source: "-Mother Teresa",
+  },
+  
+  {
+    quote: "\"When you reach the end of your rope, tie a knot in it and hang on.\"",
+    source: "-Franklin D. Roosevelt",
+    citation: "https://philosiblog.com/2014/01/30/when-you-get-to-the-end-of-your-rope-tie-a-knot-in-it-and-hang-on/",
+    year: "1919"
+  },
 
+  {
+    quote: "\"Whoever is happy will make others happy too.\"",
+    source: "-Anne Frank",
+    citation: "https://www.citatum.org/book/Anne_Frank_-_The_Diary_of_a_Young_Girl",
+    year: "1947"
+  },
 
+  {
+    quote: "\"You will face many defeats in life, but never let yourself be defeated.\"",
+    source: "-Maya Angelou",
+  },
+
+  {
+    quote: "\"In the end, it's not the years in your life that count. It's the life in your years.\"",
+    source: "-Abraham Lincoln",
+  }
+];
 
 /***
  * `getRandomQuote` function
+ * Random number associated with the number of objects in array
 ***/
+const getRandomQuote = (arr) => {
+	let randomNumber = Math.floor(Math.random() * quotes.length);
+	return quotes[randomNumber];
+}
 
 
 
 /***
  * `printQuote` function
+ * adds randomly selected quote to the DOM
+ * if the selected object has .year or .citation it'll add the HTML for it
+ * then it closes the HTML <p> tag and displays it on the DOM
 ***/
+function printQuote(){
+  let ranQuote = getRandomQuote();
+  let html = `<p class="quote"> ${ranQuote.quote} </p><p class="source"> ${ranQuote.source}`;
+  
+  if ( ranQuote.citation !== undefined ) {
+    html = html + `<span class="citation">${ranQuote.citation}</span>`;
+  } else if ( ranQuote.year !== undefined ) {
+    html = html + `<span class="year">${ranQuote.year}</span>`;
+  }
 
+  html = html + "</p>";
+
+  document.getElementById('quote-box').innerHTML = html;
+}
 
 
 /***
